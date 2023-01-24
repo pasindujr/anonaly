21253d0b8581c5391d68d223dddcfc5c4c6c0eda
Pasindu Ruwandeniya <pasindujayanada@gmail.com>
2023-01-24 23:49:06 +0530

    Home page designed. 🏠

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,3 +1,310 @@
 <div>
-    <h1>Home Page</h1>
+    <nav
+        class="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 "
+    >
+        <div
+            class="container px-4 mx-auto flex flex-wrap items-center justify-between"
+        >
+            <div
+                class="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start"
+            >
+                <a
+                    class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+                    href="{{ route('auth.login') }}"
+                >Login</a
+                >
+                <button
+                    class="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+                    type="button"
+                    onclick="toggleNavbar('example-collapse-navbar')"
+                >
+                    <i class="text-white fas fa-bars"></i>
+                </button>
+            </div>
+            <div
+                class="lg:flex flex-grow items-center bg-white lg:bg-transparent lg:shadow-none hidden"
+                id="example-collapse-navbar"
+            >
+                <ul class="flex flex-col lg:flex-row list-none mr-auto">
+                    <li class="flex items-center">
+                        <a
+                            class="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                            href="{{ route('auth.register') }}"
+                        ><i
+                                class="lg:text-gray-300 text-gray-500 far fa-file-alt text-lg leading-lg mr-2"
+                            ></i>
+                            Register</a
+                        >
+                    </li>
+                </ul>
+                <ul class="flex flex-col lg:flex-row list-none lg:ml-auto">
+                    <li class="flex items-center">
+                        <a
+                            class="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                            href="#pablo"
+                        ><i
+                                class="lg:text-gray-300 text-gray-500 fab fa-facebook text-lg leading-lg "
+                            ></i
+                            ><span class="lg:hidden inline-block ml-2">Share</span></a
+                        >
+                    </li>
+                    <li class="flex items-center">
+                        <a
+                            class="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                            href="#pablo"
+                        ><i
+                                class="lg:text-gray-300 text-gray-500 fab fa-twitter text-lg leading-lg "
+                            ></i
+                            ><span class="lg:hidden inline-block ml-2">Tweet</span></a
+                        >
+                    </li>
+                    <li class="flex items-center">
+                        <a
+                            class="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                            href="#pablo"
+                        ><i
+                                class="lg:text-gray-300 text-gray-500 fab fa-github text-lg leading-lg "
+                            ></i
+                            ><span class="lg:hidden inline-block ml-2">Star</span></a
+                        >
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <main>
+        <div
+            class="relative pt-16 pb-32 flex content-center items-center justify-center"
+            style="min-height: 75vh;"
+        >
+            <div
+                class="absolute top-0 w-full h-full bg-center bg-cover"
+                style='background-image: url("https://i0.wp.com/www.rajesh-mathur.com/wp-content/uploads/2020/10/Screen-Shot-2020-10-13-at-8.32.49-pm.png?resize=1080%2C675&ssl=");'
+            >
+          <span
+              id="blackOverlay"
+              class="w-full h-full absolute opacity-75 bg-black"
+          ></span>
+            </div>
+            <div class="container relative mx-auto">
+                <div class="items-center flex flex-wrap">
+                    <div class="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
+                        <div class="pr-12">
+                            <h1 class="text-white font-semibold text-5xl">
+                                Anonaly
+                            </h1>
+                            <p class="mt-4 text-lg text-gray-300">
+                                Give and receive anonymous feedback
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div
+                class="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden"
+                style="height: 70px;"
+            >
+                <svg
+                    class="absolute bottom-0 overflow-hidden"
+                    xmlns="http://www.w3.org/2000/svg"
+                    preserveAspectRatio="none"
+                    version="1.1"
+                    viewBox="0 0 2560 100"
+                    x="0"
+                    y="0"
+                >
+                    <polygon
+                        class="text-gray-300 fill-current"
+                        points="2560 0 2560 100 0 100"
+                    ></polygon>
+                </svg>
+            </div>
+        </div>
+        <section class="pb-20 bg-gray-300 -mt-24">
+            <div class="container mx-auto px-4">
+                <div class="flex flex-wrap">
+                    <div class="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center">
+                        <div
+                            class="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg"
+                        >
+                            <div class="px-4 py-5 flex-auto">
+                                <div
+                                    class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-red-400"
+                                >
+                                    <i class="fas fa-award"></i>
+                                </div>
+                                <h6 class="text-xl font-semibold">Unique URL</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="w-full md:w-4/12 px-4 text-center">
+                        <div
+                            class="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg"
+                        >
+                            <div class="px-4 py-5 flex-auto">
+                                <div
+                                    class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-blue-400"
+                                >
+                                    <i class="fas fa-retweet"></i>
+                                </div>
+                                <h6 class="text-xl font-semibold">Anonymous Feedback</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="pt-6 w-full md:w-4/12 px-4 text-center">
+                        <div
+                            class="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg"
+                        >
+                            <div class="px-4 py-5 flex-auto">
+                                <div
+                                    class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-green-400"
+                                >
+                                    <i class="fas fa-fingerprint"></i>
+                                </div>
+                                <h6 class="text-xl font-semibold">Manage your feedbacks</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <footer class="relative bg-gray-300 pt-8 pb-6">
+        <div
+            class="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20"
+            style="height: 80px;"
+        >
+            <svg
+                class="absolute bottom-0 overflow-hidden"
+                xmlns="http://www.w3.org/2000/svg"
+                preserveAspectRatio="none"
+                version="1.1"
+                viewBox="0 0 2560 100"
+                x="0"
+                y="0"
+            >
+                <polygon
+                    class="text-gray-300 fill-current"
+                    points="2560 0 2560 100 0 100"
+                ></polygon>
+            </svg>
+        </div>
+        <div class="container mx-auto px-4">
+            <div class="flex flex-wrap">
+                <div class="w-full lg:w-6/12 px-4">
+                    <h4 class="text-3xl font-semibold">Let's keep in touch!</h4>
+                    <h5 class="text-lg mt-0 mb-2 text-gray-700">
+                        Find us on any of these platforms, we respond 1-2 business days.
+                    </h5>
+                    <div class="mt-6">
+                        <button
+                            class="bg-white text-blue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-3"
+                            type="button"
+                        >
+                            <i class="flex fab fa-twitter"></i></button
+                        >
+                        <button
+                            class="bg-white text-blue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-3"
+                            type="button"
+                        >
+                            <i class="flex fab fa-facebook-square"></i></button
+                        >
+                        <button
+                            class="bg-white text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-3"
+                            type="button"
+                        >
+                            <i class="flex fab fa-dribbble"></i></button
+                        >
+                        <button
+                            class="bg-white text-gray-900 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-3"
+                            type="button"
+                        >
+                            <i class="flex fab fa-github"></i>
+                        </button>
+                    </div>
+                </div>
+                <div class="w-full lg:w-6/12 px-4">
+                    <div class="flex flex-wrap items-top mb-6">
+                        <div class="w-full lg:w-4/12 px-4 ml-auto">
+                <span
+                    class="block uppercase text-gray-600 text-sm font-semibold mb-2"
+                >Useful Links</span
+                >
+                            <ul class="list-unstyled">
+                                <li>
+                                    <a
+                                        class="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
+                                        href="https://www.creative-tim.com/presentation"
+                                    >About Us</a
+                                    >
+                                </li>
+                                <li>
+                                    <a
+                                        class="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
+                                        href="https://blog.creative-tim.com"
+                                    >Blog</a
+                                    >
+                                </li>
+                                <li>
+                                    <a
+                                        class="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
+                                        href="https://www.github.com/creativetimofficial"
+                                    >Github</a
+                                    >
+                                </li>
+                                <li>
+                                    <a
+                                        class="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
+                                        href="https://www.creative-tim.com/bootstrap-themes/free"
+                                    >Free Products</a
+                                    >
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="w-full lg:w-4/12 px-4">
+                <span
+                    class="block uppercase text-gray-600 text-sm font-semibold mb-2"
+                >Other Resources</span
+                >
+                            <ul class="list-unstyled">
+                                <li>
+                                    <a
+                                        class="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
+                                        href="https://github.com/creativetimofficial/argon-design-system/blob/master/LICENSE.md"
+                                    >MIT License</a
+                                    >
+                                </li>
+                                <li>
+                                    <a
+                                        class="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
+                                        href="https://creative-tim.com/terms"
+                                    >Terms &amp; Conditions</a
+                                    >
+                                </li>
+                                <li>
+                                    <a
+                                        class="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
+                                        href="https://creative-tim.com/privacy"
+                                    >Privacy Policy</a
+                                    >
+                                </li>
+                                <li>
+                                    <a
+                                        class="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
+                                        href="https://creative-tim.com/contact-us"
+                                    >Contact Us</a
+                                    >
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr class="my-6 border-gray-400"/>
+            <div
+                class="flex flex-wrap items-center md:justify-between justify-center"
+            >
+            </div>
+        </div>
+    </footer>
 </div>
